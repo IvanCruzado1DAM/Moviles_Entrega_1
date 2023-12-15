c815af532cafb5ac62693a64110726963fd6559a
Fernando Villalba <fervillalbita@gmail.com>
2023-12-15 14:14:05 +0100

    implementado  añadir elemento

--- lib/services/element_srervices.dart
+++ lib/services/element_srervices.dart
@@ -7,11 +7,61 @@ import 'package:mindcare/models/elements.dart';
 import 'package:mindcare/services/user_services.dart';
 
 class ElementService extends ChangeNotifier {
+  static String idUser = '';
+  static String type = '';
   final String baseURL = 'mindcare.allsites.es';
   final storage = const FlutterSecureStorage();
   final List<ElementData> elements = [];
 
   bool isLoading = true;
+
+  Future newElement(
+    int? emotionId,
+    int? moodId,
+    String? description,
+    String typeUser,
+    String idUser,
+    String type,
+    String date,
+  ) async {
+    final Map<String, dynamic> elementData = {
+      'id_user': idUser,
+      'type_user': typeUser,
+      'type': type,
+      // aqui el tipo date (en un futuro) debemos hacerle un formatter y en register Screen usar este campo
+      'date': date,
+    };
+    if (moodId != null) {
+      elementData['mood_id'] = moodId;
+    }
+    if (moodId != null) {
+      elementData['emotion_id'] = emotionId;
+    }
+    if (description != null) {
+      elementData['description'] = description;
+    }
+    print(elementData);
+
+    final url = Uri.http(baseURL, '/public/api/newElement', {});
+    String? authToken = await readToken();
+
+    final response = await http.post(url,
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $authToken',
+        },
+        body: json.encode(elementData));
+
+    final Map<String, dynamic> decoded = json.decode(response.body);
+
+    if (decoded['success'] == true) {
+      ElementService.idUser = decoded['data']['id_user'].toString();
+      ElementService.type = decoded['data']['type'].toString();
+      return 'success';
+    } else {
+      return 'error';
+    }
+  }
 
   Future<String> readToken() async {
     return await storage.read(key: 'token') ?? '';
@@ -19,8 +69,9 @@ class ElementService extends ChangeNotifier {
 
   Future<List<ElementData>> getElements() async {
     final String token = await readToken();
-    final Uri url = Uri.http(baseURL, '/public/api/elements', {"id": UserService.userId.toString()});
-   
+    final Uri url = Uri.http(
+        baseURL, '/public/api/elements', {"id": UserService.userId.toString()});
+
     try {
       isLoading = true;
       notifyListeners();
@@ -34,19 +85,20 @@ class ElementService extends ChangeNotifier {
       );
 
       if (resp.statusCode == 200) {
-        elements.clear(); // Limpiamos la lista antes de agregar nuevos elementos
+        elements
+            .clear(); // Limpiamos la lista antes de agregar nuevos elementos
 
         final Map<String, dynamic> decodedData = json.decode(resp.body);
         var elementData = ElementData.fromJson(decodedData);
 
-       // Verificar si elementData.data es null o está vacío antes de agregar elementos
-      if (elementData.data != null && elementData.data!.isNotEmpty) {
-        elements.clear(); // Limpiamos la lista antes de agregar nuevos elementos
-        elements.addAll(elementData.data!); // Agregamos todos los elementos
-      } else {
-        print('El campo "data" está vacío o es nulo.');
-      }
-
+        // Verificar si elementData.data es null o está vacío antes de agregar elementos
+        if (elementData.data != null && elementData.data!.isNotEmpty) {
+          elements
+              .clear(); // Limpiamos la lista antes de agregar nuevos elementos
+          elements.addAll(elementData.data!); // Agregamos todos los elementos
+        } else {
+          print('El campo "data" está vacío o es nulo.');
+        }
 
         isLoading = false;
         notifyListeners();
@@ -61,8 +113,5 @@ class ElementService extends ChangeNotifier {
       print('Error: $error');
       throw Exception('Error: $error');
     }
-    
   }
-  
-  
 }
